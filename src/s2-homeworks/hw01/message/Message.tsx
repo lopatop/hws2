d21d5b1e2ddee201cf81128a4dca5b23ea1ecdbb
lopatop <pavellopato327@gmail.com>
2025-02-01 18:35:49 +0100

    Message

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import s from './Message.module.css'
+import {MessageType} from "../HW1";
 
 // нужно создать правильный тип вместо any
-export type MessagePropsType = any
+export type MessagePropsType = {
+    message: MessageType
+}
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
@@ -11,17 +14,18 @@ const Message = (props: MessagePropsType) => {
             <div className={s.imageAndText}>
                 <img
                     id={'hw1-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
+                    src={props.message.user.avatar}
                 />
                 <div className={s.text}>
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
+                        {props.message.user.name}
                         {/*создаёт студент*/}
 
                         {/**/}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
+                        {props.message.message.text}
+
                         {/*создаёт студент*/}
 
                         {/**/}
@@ -29,6 +33,8 @@ const Message = (props: MessagePropsType) => {
                 </div>
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
+                {props.message.message.time}
+
                 {/*создаёт студент*/}
 
                 {/**/}
