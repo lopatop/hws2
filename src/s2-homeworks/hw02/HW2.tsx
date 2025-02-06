7fdf17f47e903775b27836da29090910ab835fb7
lopatop <pavellopato327@gmail.com>
2025-02-07 00:48:18 +0100

    function filterAffairs and propsType

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -17,16 +17,16 @@ import s2 from '../../s1-main/App.module.css'
 * */
 
 // types
-export type AffairPriorityType = any // need to fix any
+export type AffairPriorityType = 'low' | 'high'| 'middle'
 export type AffairType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: number
+    name: string
     priority: AffairPriorityType
 }
 export type FilterType = 'all' | AffairPriorityType
 
 // constants
-const defaultAffairs: any = [ // need to fix any
+const defaultAffairs: AffairType[] = [ // need to fix any
     {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
@@ -35,10 +35,13 @@ const defaultAffairs: any = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-
-
-    return affairs // need to fix
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => { // need to fix any
+    if (filter === "all") return affairs
+    if (filter === "high") return affairs.filter(f=>f.priority === 'high')
+    if (filter === "low") return affairs.filter(f=>f.priority === 'low')
+    if (filter === "middle") return affairs.filter(f=>f.priority === 'middle')
+    // return affairs.filter(f=> f.priority === filter)
+    return affairs
 }
 export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
 
