cc3cdfc2c627c55bc03e9eaa8a732aad8aa689a4
lopatop <pavellopato327@gmail.com>
2025-03-06 21:32:16 +0100

    setEditMode()

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -41,21 +41,24 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
     spanProps || {}
 
     const onEnterCallback = () => {
+        onEnter?.();
+        setEditMode(false);
+
+
         // выключить editMode при нажатии Enter // делают студенты
 
-        onEnter?.()
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
 
         onBlur?.(e)
+        setEditMode(false);
     }
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
-        // включить editMode при двойном клике // делают студенты
-
         onDoubleClick?.(e)
+        setEditMode(true);
     }
 
     const spanClassName = s.span
