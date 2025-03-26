fa538559698f6bca4d81bc0c057e68ea49fe2ec3
lopatop <pavellopato327@gmail.com>
2025-03-26 22:19:28 +0100

    refactor Clock

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,7 +33,7 @@ function Clock() {
 
 
     const stringTime = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth()).padStart(2, '0')}.${String(date.getFullYear()).padStart(2, '0')}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${String(date.getDate()).padStart(2, '0')}.${String(date.getMonth()+1).padStart(2, '0')}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = new Intl.DateTimeFormat('en-GB', {weekday: 'long'}).format(new Date())|| <br/> // пишут студенты
