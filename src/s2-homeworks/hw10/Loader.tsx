33ad21b67f0f1c3727be6b1310618a45f1a06355
lopatop <pavellopato327@gmail.com>
2025-04-14 09:48:33 +0200

    Loader

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,18 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+    <svg
+        className={s.loader}
+        width="50"
+        height="50"
+        viewBox="0 0 50 50"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <circle
+            cx="25"
+            cy="25"
+            r="20"
+            fill="none"
+        />
+    </svg>
+)
