97a0cc66d2af76644e6f2308eb02f72d442b263a
lopatop <pavellopato327@gmail.com>
2025-04-30 23:30:05 +0200

    add logic in onChangeTextCallback

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -33,8 +33,18 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
 
         if (onDebouncedChange) {
             // делает студент
+            if (timerId) {
+                clearTimeout(timerId);
+            }
 
+
+            const newTimerId = setTimeout(() => {
+                onDebouncedChange(value);  // Вызовем onDebouncedChange после задержки
+            }, 1500);
+
+            setTimerId(Number(newTimerId));
             // остановить предыдущий таймер
+
             // запустить новый на 1500ms, в котором вызовется функция
 
             //
