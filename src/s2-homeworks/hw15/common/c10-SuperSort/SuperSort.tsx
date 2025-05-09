e1d3bc52a721a9ba393d116d2eb321b7d03c587d
lopatop <pavellopato327@gmail.com>
2025-05-09 12:01:12 +0200

    SuperSort

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
 
 // добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+const downIcon = <ArrowDropDownIcon/>
+const upIcon = <ArrowDropUpIcon/>
+const noneIcon = <UnfoldMoreIcon/>
 
 export type SuperSortPropsType = {
     id?: string
@@ -14,7 +17,12 @@ export type SuperSortPropsType = {
 
 export const pureChange = (sort: string, down: string, up: string) => {
     // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+
+    if (sort === '') return down;
+    if (sort === down) return up;
+    if (sort === up) return '';
+    return down;
+
 }
 
 const SuperSort: React.FC<SuperSortPropsType> = (
@@ -41,12 +49,10 @@ const SuperSort: React.FC<SuperSortPropsType> = (
             onClick={onChangeCallback}
         >
             {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
+            <span id={id + '-icon-' + sort}>
+                {icon}
+            </span>
 
-            {icon} {/*а это убрать*/}
         </span>
     )
 }
